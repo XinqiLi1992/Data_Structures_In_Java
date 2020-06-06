722914d8a7258436684b36f63fd2d60a8803762e
Xinqi Li <rickyli1992@gmail.com>
2020-06-06 16:59:49 +0100

    Array Implementation

---
+++ data-structures-in-java/src/main/java/Array/Array.java
@@ -0,0 +1,140 @@
+package Array;
+
+public class Array<E> {
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Cannot add. Index is invalid.");
+        }
+
+        if (size == data.length) {
+            resize(data.length * 2);
+        }
+
+        data[index] = e;
+        size++;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Cannot get. Index is invalid");
+        }
+        return data[index];
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Cannot set. Index is invalid");
+        }
+        data[index] = e;
+    }
+
+    public boolean contains(E e) {
+        for(int i = 0; i < size; i++) {
+            if (get(i).equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(E e) {
+        for(int i = 0; i < size; i++) {
+            if (get(i).equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public boolean removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Cannot remove. Index is invalid.");
+        }
+        E result = data[index];
+        data[index] = null;
+        size--;
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return result;
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Array: ");
+        stringBuilder.append('[');
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append(data[i]);
+            if (i != size - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append(']');
+        return stringBuilder.toString();
+    }
+}
