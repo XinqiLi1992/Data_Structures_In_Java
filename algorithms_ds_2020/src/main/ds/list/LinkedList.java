ee5e0f52181fc107946b921692b6c9022a141aab
Xinqi Li <rickyli1992@gmail.com>
2020-09-07 23:01:40 +0100

    Set implementation

--- algorithms_ds_2020/src/main/ds/list/LinkedList.java
+++ algorithms_ds_2020/src/main/ds/list/LinkedList.java
@@ -131,6 +131,22 @@ public class LinkedList<E> {
         return remove(size - 1);
     }
 
+    public void removeElement(E e) {
+        Node prev = dummyHead;
+        while(prev.next != null) {
+            if (e.equals(prev.next.e)) {
+                break;
+            }
+            prev = prev.next;
+        }
+
+        if (prev.next != null) {
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
---
+++ algorithms_ds_2020/src/main/ds/set/LinkedListSet.java
@@ -0,0 +1,39 @@
+package main.ds.set;
+
+import main.ds.list.LinkedList;
+
+public class LinkedListSet<E extends Comparable<E>> implements Set<E> {
+
+    private LinkedList<E> list;
+
+    private LinkedListSet() {
+        list = new LinkedList<>();
+    }
+
+    @Override
+    public void add(E e) {
+        if (!list.contains(e)) {
+            list.addFirst(e);
+        }
+    }
+
+    @Override
+    public void remove(E e) {
+        list.removeElement(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return list.contains(e);
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+}
